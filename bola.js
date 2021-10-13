30714d286b05a6b823bd423b250579c0b0ec4de8
Alaneaz <88462919+Alaneaz@users.noreply.github.com>
2021-10-14 01:30:25 +0700

    Update and rename README.md to bola.js

---
+++ bola.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+ $("#btnSubmit").click(function(){
+        $(".asu").css("background", ""); //passing value as null.
+        $(".asuballhk").css("background", ""); //passing value as null.
+        $(".asux3").css("background", ""); //passing value as null.
+    }); 
+}); 
