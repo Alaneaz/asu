7129a0ce432101885c8781fca096ed876f9fcf43
Alaneaz <88462919+Alaneaz@users.noreply.github.com>
2022-06-06 13:07:07 +0700

    Update bola.js

--- bola.js
+++ bola.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
         $(".asu").css("background", ""); //passing value as null.
         $(".asuballhk").css("background", ""); //passing value as null.
         $(".asu3").css("background", ""); //passing value as null. 
-        $(".asu4").css("background", ""); //passing value as null.
+        $(".singa").css("background", ""); //passing value as null.
+        $(".hkgball").css("background", ""); //passing value as null.
     }); 
 }); 
